d7a169e9a25daaf13b5e83aba0dad9c0c12b33bf
AlekseySV12 <alekseysv@abv.bg>
2020-10-14 14:03:42 +0000

    .

--- src/exercise3/NumberSearch.java
+++ src/exercise3/NumberSearch.java
@@ -5,9 +5,13 @@ public class NumberSearch {
         int count=0;
         for(int i=100;i<=1000;i++){
             if(i%30==0){
-              if(count<10){
+              if(count<9){
               count++;
               System.out.print(i+" ");
+              }
+              else if(count==9){
+              count++;
+              System.out.print(i);
               }
               
               if(count==10){
